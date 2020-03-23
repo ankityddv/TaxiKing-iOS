dbbe4ef689a21bd9e7af735d5db6e46f295e5fa3
Ankit Yadav <yadavankit840@gmail.com>
2020-03-23 16:17:44 +0530

    Initial Commit

---
+++ TaxiKingTests/TaxiKingTests.swift
@@ -0,0 +1,34 @@
+//
+//  TaxiKingTests.swift
+//  TaxiKingTests
+//
+//  Created by ANKIT YADAV on 23/03/20.
+//  Copyright © 2020 ANKIT YADAV. All rights reserved.
+//
+
+import XCTest
+@testable import TaxiKing
+
+class TaxiKingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
